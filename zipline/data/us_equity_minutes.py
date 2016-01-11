decfed2e7d4262bf9b9599644fedc916a98c915e
Eddie Hebert <ehebert@quantopian.com>
2016-01-11 15:16:40 -0500

    Save point to pull over writer.

--- zipline/data/us_equity_minutes.py
+++ zipline/data/us_equity_minutes.py
@@ -11,14 +11,166 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+from abc import (
+    ABCMeta,
+    abstractmethod,
+)
 import bcolz
+from bcolz import ctable
+from datetime import datetime
+import numpy as np
+from os.path import join
 import json
 import os
 import pandas as pd
+from six import with_metaclass
+
+from zipline.finance.trading import TradingEnvironment
+from zipline.utils import tradingcalendar
 
 MINUTES_PER_DAY = 390
 
+_writer_env = TradingEnvironment()
+
 METADATA_FILENAME = 'metadata.json'
+
+
+def write_metadata(directory, first_trading_day):
+    metadata_path = os.path.join(directory, METADATA_FILENAME)
+
+    metadata = {
+        'first_trading_day': str(first_trading_day.date())
+    }
+
+    with open(metadata_path, 'w') as fp:
+        json.dump(metadata, fp)
+
+
+def _bcolz_minute_index(trading_days):
+    minutes = np.zeros(len(trading_days) * MINUTES_PER_DAY,
+                       dtype='datetime64[ns]')
+    market_opens = tradingcalendar.open_and_closes.market_open
+    mask = market_opens.index.slice_indexer(start=trading_days[0],
+                                            end=trading_days[-1])
+    opens = market_opens[mask]
+
+    deltas = np.arange(0, MINUTES_PER_DAY, dtype='timedelta64[m]')
+    for i, market_open in enumerate(opens):
+        start = market_open.asm8
+        minute_values = start + deltas
+        start_ix = MINUTES_PER_DAY * i
+        end_ix = start_ix + MINUTES_PER_DAY
+        minutes[start_ix:end_ix] = minute_values
+    return pd.to_datetime(minutes, utc=True, box=True)
+
+
+class BcolzMinuteBarWriter(with_metaclass(ABCMeta)):
+    """
+    Class capable of writing minute OHLCV data to disk into bcolz format.
+    """
+    @property
+    def first_trading_day(self):
+        return self._first_trading_day
+
+    @abstractmethod
+    def gen_frames(self, assets):
+        """
+        Return an iterator of pairs of (asset_id, pd.dataframe).
+        """
+        raise NotImplementedError()
+
+    def write(self, directory, assets, sid_path_func=None):
+        _iterator = self.gen_frames(assets)
+
+        return self._write_internal(directory, _iterator,
+                                    sid_path_func=sid_path_func)
+
+    def full_minutes_for_days(self, dt1, dt2):
+        start_date = _writer_env.normalize_date(dt1)
+        end_date = _writer_env.normalize_date(dt2)
+
+        trading_days = _writer_env.days_in_range(start_date, end_date)
+        return _bcolz_minute_index(trading_days)
+
+    def _write_internal(self, directory, iterator, sid_path_func=None):
+        first_trading_day = self.first_trading_day
+
+        write_metadata(directory, first_trading_day)
+
+        first_open = pd.Timestamp(
+            datetime(
+                year=first_trading_day.year,
+                month=first_trading_day.month,
+                day=first_trading_day.day,
+                hour=9,
+                minute=31
+            ), tz='US/Eastern').tz_convert('UTC')
+
+        all_minutes = None
+
+        for asset_id, df in iterator:
+            if sid_path_func is None:
+                path = join(directory, "{0}.bcolz".format(asset_id))
+            else:
+                path = sid_path_func(directory, asset_id)
+
+            os.makedirs(path)
+
+            last_dt = df.index[-1]
+
+            if all_minutes is None:
+                all_minutes = \
+                    self.full_minutes_for_days(first_open, last_dt)
+                minutes = all_minutes
+            else:
+                if df.index[-1] in all_minutes:
+                    mask = all_minutes.slice_indexer(end=last_dt)
+                    minutes = all_minutes[mask]
+                else:
+                    # Need to extend all minutes from open after last value
+                    # in all_minutes to the last_dt.
+                    next_open, _ = _writer_env.next_open_and_close(
+                        all_minutes[-1])
+                    to_append = self.full_minutes_for_days(next_open, last_dt)
+                    all_minutes = all_minutes.append(to_append)
+                    minutes = all_minutes
+
+            minutes_count = len(minutes)
+
+            open_col = np.zeros(minutes_count, dtype=np.uint32)
+            high_col = np.zeros(minutes_count, dtype=np.uint32)
+            low_col = np.zeros(minutes_count, dtype=np.uint32)
+            close_col = np.zeros(minutes_count, dtype=np.uint32)
+            vol_col = np.zeros(minutes_count, dtype=np.uint32)
+
+            dt_ixs = np.searchsorted(minutes.values, df.index.values)
+
+            open_col[dt_ixs] = df.open.values.astype(np.uint32)
+            high_col[dt_ixs] = df.high.values.astype(np.uint32)
+            low_col[dt_ixs] = df.low.values.astype(np.uint32)
+            close_col[dt_ixs] = df.close.values.astype(np.uint32)
+            vol_col[dt_ixs] = df.volume.values.astype(np.uint32)
+
+            ctable(
+                columns=[
+                    open_col,
+                    high_col,
+                    low_col,
+                    close_col,
+                    vol_col,
+                ],
+                names=[
+                    "open",
+                    "high",
+                    "low",
+                    "close",
+                    "volume",
+                ],
+                rootdir=path,
+                mode='w'
+            )
 
 
 class BcolzMinuteBarReader(object):
